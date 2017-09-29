adaa09c1aa6b600c167b07843897b31a80322231
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-09-29 15:27:33 +0200

    fix petit probleme

--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -5,17 +5,21 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String argv[]) {
-		float[] dataset = new float[16];
-		Random r = new Random();
-		for( int i = 0; i < dataset.length; i++){
-			dataset[i] = r.nextFloat();
-		}
+		float[] dataset = new float[4];
+//		Random r = new Random();
+//		for( int i = 0; i < dataset.length; i++){
+//			dataset[i] = r.nextFloat();
+//		}
+		dataset[0] = 9;
+		dataset[1] = 7;
+		dataset[2] = 3;
+		dataset[3] = 5;
 		
 		for(int i=0; i < dataset.length; i++){
 			System.out.println("dataset " + i + " : " + dataset[i]);
 		}
 		
-		float[] data_out = Decomposition.decomposition(dataset, "full", 4);
+		float[] data_out = Decomposition.decomposition(dataset, "full", 2);
 		
 		for(int i=0; i < data_out.length; i++){
 			System.out.println("data_out " + i + " : " + data_out[i]);
