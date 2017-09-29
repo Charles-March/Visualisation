5137b7f66bd3fc426033a45076001abc259dfb64
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-09-29 14:08:57 +0200

    tst

---
+++ src/tp1/Decomposition.java
@@ -0,0 +1,16 @@
+package tp1;
+
+public class Decomposition {
+
+	public static int[] decomposition(int[] data_in, String[] args){
+		int[] data_out = new int[10];
+		switch (args[0]){
+			case "1":
+				break;
+		}
+				
+		return data_out;
+	}
+	
+	public int[] algo (int)
+}
