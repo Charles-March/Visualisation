ec26362c57b44c0510a356a3027c08b9d2a3fe0b
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-09-29 15:12:10 +0200

    1 et 3

--- src/tp1/Decomposition.java
+++ src/tp1/Decomposition.java
@@ -1,31 +1,43 @@
 package tp1;
 
 public class Decomposition {
-	
 
-	public static float[] decomposition(float[] data_in, String[] args){
+	public static float[] decomposition(float[] data_in, String arg, int left){
 		float[] data_out = new float[data_in.length];
-		switch (args[0]){
+		switch (arg){
 			case "1":
-				data_out = algo(data_in, 1);
-				System.out.println(data_out);
+				data_out = algo(data_in, 1, left);
 				break;
+			default:
+				data_out = algo(data_in, 1, left);
 		}
 				
 		return data_out;
 	}
 	
-	public static float[] algo (float[] data_in, int rank){
+	public static float[] algo (float[] data_in, int rank, int left){
 		int size_of_data = data_in.length;
 		float[] data_out = new float[size_of_data];
+		data_out = data_in;
 		// composition de la premiere partie des données
-		for(int i=0; i < size_of_data/2*rank; i++){
-			data_out[i] = 1/2*(data_in[2*i] + data_in[2*i+1]);
+		System.out.println(size_of_data);
+		System.out.println("rank est " + rank);
+		System.out.println("rank" + size_of_data/(2*rank));
+		for(int i=0; i < (size_of_data/(2*rank)); i++){
+			data_out[i] = (float) 0.5*(data_in[2*i] + data_in[2*i+1]);
 		}
 		// composition de la seconde partie des données
-		for(int i = size_of_data/2*rank; i < size_of_data; i++){
-			data_out[i] = 1/2*(data_in[2*i] - data_in[2*i+1]);
+		int j = 0;
+		for(int i = size_of_data/(2*rank); i < size_of_data/rank; i++){
+			data_out[i] = (float) 0.5*(data_in[2*j] - data_in[2*j+1]);
+			j++;
 		}
-		return data_out;
+		if (left == 1){
+			return data_out;
+		} else {
+			rank = rank + 1;
+			left = left - 1;
+			return (algo(data_out, rank, left));
+		}
 	}
 }
