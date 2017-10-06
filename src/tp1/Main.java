ea306f7450bc8c55370d61d9fcf346392e7bc5e7
Stractus <charles-marchand@hotmail.fr>
2017-10-06 15:36:44 +0200

    fazeazefefa

--- src/tp1/Main.java
+++ src/tp1/Main.java
@@ -3,14 +3,15 @@ package tp1;
 public class Main {
 
 	public static void main(String argv[]) {
-		double[] dataset =Tools.generate_tab(128, 1, 10);
+		double[] dataset =Tools.generate_tab(32, 1, 10);
 		Reconstruction.printT("dataset",dataset);
-		double[] data_out = Decomposition.decomposition(dataset,"all", Tools.log2(dataset.length));
+		double[] data_out = Decomposition.decomposition(dataset,"1.0", Tools.log2(dataset.length));
+		double[] details=data_out.clone();
 		Reconstruction.printT("dataout",data_out);
 		data_out=Reconstruction.reconstruit(data_out);
 		Reconstruction.printT("data_out",data_out);
 		System.out.println("Erreur :" + CalculErreur.Erreur(dataset,data_out));
-		
+		Tools.export(dataset, data_out,details);
 		}
 	}
 	
--- src/tp1/Tools.java
+++ src/tp1/Tools.java
@@ -1,5 +1,14 @@
 package tp1;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Tools {
 	public static int log2(int a){
 		int b=0;
@@ -20,5 +29,64 @@ public class Tools {
 				tab[i]= min + (int)(Math.random() * max); 
 			}
 		return tab;
+	}
+	
+	public static boolean export(double[] dataIn, double[] dataOut,double[] details){
+		try {
+			PrintWriter writerCoef = new PrintWriter("CoefDetails.csv", "UTF-8");
+			PrintWriter writerGraph = new PrintWriter("Erreur.csv","UTF-8");
+			HashMap<Double, Integer> map=new HashMap<Double,Integer>();
+			for(int i=0;i<details.length;i++){
+				if(map.containsKey(details[i])){
+					map.put(details[i], map.get(details[i]+1));
+				}
+				else{
+					map.put(details[i], 1);
+				}
+			}
+			System.out.println(map.size());
+			Iterator it = map.entrySet().iterator();
+		    while (it.hasNext()) {
+		        Map.Entry pair = (Map.Entry)it.next();
+		        System.out.print(pair.getKey()+",");
+		        writerCoef.print(pair.getKey()+",");
+		        it.remove(); // avoids a ConcurrentModificationException
+		    }
+		    
+		    writerCoef.println();
+		    
+		    Iterator it2 = map.entrySet().iterator();
+		    while (it2.hasNext()) {
+		        Map.Entry pair = (Map.Entry)it2.next();
+		        writerCoef.print(pair.getValue()+",");
+		        System.out.print(pair.getValue()+",");
+		        it2.remove(); // avoids a ConcurrentModificationException
+		    }
+		    writerCoef.println();
+		    writerCoef.close();
+			/*
+			for(int i=0;i<dataIn.length;i++){
+				writer.print(dataIn[i]+",");
+				
+			}
+			writer.println("");
+			for(int i=0;i<dataOut.length;i++){
+				writer.print(dataOut[i]+",");
+				
+			}
+			writer.println("");	
+			
+			writer.close();
+			*/
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		
+		return true;
 	}
 }
